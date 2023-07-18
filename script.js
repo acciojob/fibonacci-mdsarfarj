3107ce7502b23b219b1287d55f62717571b5244a
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-07-18 14:58:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function fibonacci(num) {
+function fibonacci(n) {
 // your code here
-}
-
+	if (num <= 0) {
+    return null;
+  } else if (num === 1) {
+    return 0;
+  } else if (num === 2) {
+    return 1;
+  } else {
+    let a = 0;
+    let b = 1;
+    for (let i = 3; i <= num; i++) {
+      let temp = a + b;
+      a = b;
+      b = temp;
+    }
+    return b;
+  }
+  }
+	
 module.exports = fibonacci;
